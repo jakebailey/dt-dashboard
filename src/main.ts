b850d0a0900470cb2b759682fd7d1f56cd78c7b8
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-10-07 15:23:12 -0700

    List stuff

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { Command, Option, runExit } from "clipanion";
+import { glob } from "glob";
 
 void runExit(
     class MainCommand extends Command {
-        name = Option.String();
+        dtCheckout = Option.String({ required: true });
 
         async execute() {
-            this.context.stdout.write(`Hello ${this.name}!\n`);
+            this.context.stdout.write(`DefinitelyTyped checkout at ${this.dtCheckout}!\n`);
+
+            const p = await getTopLevelPackages(this.dtCheckout);
+            this.context.stdout.write(`Found ${p.length} packages!\n`);
+
+            // TODO: use DT infra instead of glob
         }
     },
 );
+
+function getTopLevelPackages(dtCheckout: string) {
+    return glob(`types/*`, { cwd: dtCheckout, absolute: true });
+}
