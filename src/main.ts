336ad5a8f2be3210422ec8febc21476c6d7d46c8
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-10-07 19:55:18 -0700

    Progress

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable unicorn/no-nested-ternary */
+/* eslint-disable unicorn/no-null */
 import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
@@ -12,7 +14,7 @@ import {
 import { parseHeaderOrFail } from "@definitelytyped/header-parser";
 import { Command, Option, runExit } from "clipanion";
 import fetch from "node-fetch";
-import * as semver from "semver";
+import { SemVer } from "semver";
 
 void runExit(
     { enableCapture: true },
@@ -36,7 +38,7 @@ void runExit(
             allTypingsData.sort((a, b) => compareComparableValues(a.subDirectoryPath, b.subDirectoryPath));
 
             for (const pkg of allTypingsData) {
-                await checkPackage(definitelyTypedPath, pkg);
+                await withCache(this.cachePath, pkg, (cached) => checkPackage(definitelyTypedPath, cached, pkg));
             }
         }
     },
@@ -55,6 +57,35 @@ function getAllDefinitions(definitelyTypedPath: string) {
     );
 }
 
+const CachedInfo = v.object({
+    latest: v.string(),
+    outOfDate: v.boolean(),
+    hasTypes: v.boolean(),
+}).nullable();
+type CachedInfo = v.Infer<typeof CachedInfo>;
+
+async function withCache(
+    cachePath: string,
+    data: TypingsData,
+    fn: (cached: CachedInfo | undefined) => Promise<CachedInfo | undefined>,
+) {
+    const dir = path.join(cachePath, data.subDirectoryPath[0]);
+    await fs.promises.mkdir(dir, { recursive: true });
+    const cachedPath = path.join(dir, `${data.subDirectoryPath.replace(/[/\\]/g, `@`)}.json`);
+    let cached: CachedInfo | undefined;
+    try {
+        const contents = await fs.promises.readFile(cachedPath, { encoding: `utf8` });
+        cached = CachedInfo.parse(JSON.parse(contents));
+    } catch {
+        // ignore
+    }
+
+    const result = await fn(cached);
+    if (result !== undefined) {
+        await fs.promises.writeFile(cachedPath, JSON.stringify(result, undefined, 4));
+    }
+}
+
 const PackageJSON = v.object({
     version: v.string(),
     types: v.string().optional(),
@@ -69,7 +100,11 @@ function packageJSONIsTyped(p: PackageJSON): boolean {
         || (!!p.exports && Object.values(p.exports).some((value) => typeof value !== `string` && value.types));
 }
 
-async function checkPackage(definitelyTypedPath: string, data: TypingsData) {
+async function checkPackage(
+    definitelyTypedPath: string,
+    cached: CachedInfo | undefined,
+    data: TypingsData,
+): Promise<CachedInfo | undefined> {
     // console.log(`${data.fullNpmName} ${data.unescapedName} ${data.major}.${data.minor}`);
 
     const packageRoot = path.join(definitelyTypedPath, `types`, data.subDirectoryPath);
@@ -77,37 +112,56 @@ async function checkPackage(definitelyTypedPath: string, data: TypingsData) {
     const indexDts = await fs.promises.readFile(indexDtsPath, { encoding: `utf8` });
     const header = parseHeaderOrFail(indexDtsPath, indexDts);
     if (header.nonNpm) {
-        return;
+        return undefined;
     }
 
-    const url = `https://unpkg.com/${data.unescapedName}@${data.isLatest ? `latest` : data.major}/package.json`;
+    const versionQuery = data.isLatest ? `latest`
+        : data.major === 0 ? `${data.major}.${data.minor}`
+        : `${data.major}`;
+
+    const url = `https://unpkg.com/${data.unescapedName}@${versionQuery}/package.json`;
     const result = await fetch(url);
     if (!result.ok) {
         if (result.status === 404) {
             console.log(`${data.unescapedName} not found on npm`);
-        } else {
-            console.log(`${data.unescapedName} failed to fetch ${result.status} ${result.statusText}`);
+            return null;
         }
-        return;
+        throw new Error(`${data.unescapedName} failed to fetch ${result.status} ${result.statusText}`);
     }
 
     const contents = await result.json();
-    const parsed = PackageJSON.parse(contents, { mode: `strip` });
+    const packageJSON = PackageJSON.parse(contents, { mode: `strip` });
+
+    if (packageJSON.version === cached?.latest) {
+        return cached;
+    }
+
+    let outOfDate = false;
+    let hasTypes = false;
 
     if (data.isLatest) {
-        const version = new semver.SemVer(parsed.version);
+        const version = new SemVer(packageJSON.version);
         if (data.major === 0) {
             if (version.minor > data.minor) {
                 console.log(`${data.unescapedName} is out of date`);
+                outOfDate = true;
             }
         } else {
             if (version.major > data.major) {
                 console.log(`${data.unescapedName} is out of date`);
+                outOfDate = true;
             }
         }
     }
 
-    if (packageJSONIsTyped(parsed)) {
+    if (packageJSONIsTyped(packageJSON)) {
         console.log(`${data.unescapedName} has types`);
+        hasTypes = true;
     }
+
+    return {
+        latest: packageJSON.version,
+        outOfDate,
+        hasTypes,
+    };
 }
