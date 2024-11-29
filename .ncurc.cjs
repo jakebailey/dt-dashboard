f63c29e7c6e2a250cf4aabdc609b0e1edc9f62a5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-11-28 17:22:26 -0800

    update pacote

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
     target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
         if (dependencyName === "eslint") return "minor";
-        if (dependencyName === "pacote") return "minor";
         if (dependencyName.startsWith("@typescript-eslint")) return "minor";
         if (major === "0") return "minor";
         return "latest";
